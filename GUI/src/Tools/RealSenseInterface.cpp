44d64d9f6cd1e2c47ad548d0c77f1e5988d263ae
Yutaka Takaoka <yutaka.takaoka@tri.global>
2018-05-06 23:09:12 -0700

    Added Realsense2

--- GUI/src/Tools/RealSenseInterface.cpp
+++ GUI/src/Tools/RealSenseInterface.cpp
@@ -1,24 +1,81 @@
 #include "RealSenseInterface.h"
 #include <functional>
+#include <cstring>
 
 #ifdef WITH_REALSENSE
+
+const rs2_format kStereoFormat = RS2_FORMAT_Z16;
+const int kStereoFps = 30;
+const rs2_format kColorFormat = RS2_FORMAT_RGB8;
+
 RealSenseInterface::RealSenseInterface(int inWidth,int inHeight,int inFps)
   : width(inWidth),
   height(inHeight),
   fps(inFps),
-  dev(nullptr),
+    // dev(nullptr),
   initSuccessful(true)
 {
-  if(ctx.get_device_count() == 0)
-  {
+  rs2::device_list devices = ctx.query_devices();
+  if (devices.size() == 0) {
+    std::cerr << "No device connected, please connect a RealSense device" << std::endl;
+    //To help with the boilerplate code of waiting for a device to connect
+    //The SDK provides the rs2::device_hub class
+    rs2::device_hub device_hub(ctx);
+    //Using the device_hub we can block the program until a device connects
+    dev = device_hub.wait_for_device();
+  } else if (devices.size() == 1) {
+    dev = devices[0];
+  } else {
+    std::cerr << "Connected more than 2 sensors. We assume only one sensor." << std::endl;
     errorText = "No device connected.";
     initSuccessful = false;
     return;
   }
 
-  dev = ctx.get_device(0);
-  dev->enable_stream(rs::stream::depth,width,height,rs::format::z16,fps);
-  dev->enable_stream(rs::stream::color,width,height,rs::format::rgb8,fps);
+  auto sensors = dev.query_sensors();
+  rs2::stream_profile stereo_profile;
+  rs2::stream_profile color_profile;
+  for (auto&& sensor : sensors) {
+    std::string module_name = sensor.get_info(RS2_CAMERA_INFO_NAME);
+    if (module_name == "Stereo Module") {
+      stereo = sensor;
+      auto profiles = stereo.get_stream_profiles();
+      for (auto& profile : profiles) {
+	auto video_profile = profile.as<rs2::video_stream_profile>();
+	if (video_profile.format() == kStereoFormat &&
+	    video_profile.width() == width &&
+	    video_profile.height() == height &&
+	    video_profile.fps() == fps) {
+	  stereo_profile = profile;
+	  std::cout << module_name << " ["
+		    << video_profile.format() << " "
+		    << video_profile.width() << " "
+		    << video_profile.height() << " "
+		    << video_profile.fps() << "]" << std::endl;
+	}
+      }
+    } else if (module_name == "RGB Camera") {
+      color = sensor;
+      auto profiles = color.get_stream_profiles();
+      for (auto& profile : profiles) {
+	auto video_profile = profile.as<rs2::video_stream_profile>();
+	if (video_profile.format() == kColorFormat &&
+	    video_profile.width() == width &&
+	    video_profile.height() == height &&
+	    video_profile.fps() == fps) {
+	  color_profile = profile;
+	  std::cout << module_name << " ["
+		    << video_profile.format() << " "
+		    << video_profile.width() << " "
+		    << video_profile.height() << " "
+		    << video_profile.fps() << "]" << std::endl;
+	}
+      }
+    }
+  }
+
+  stereo.open(stereo_profile);
+  color.open(color_profile);
 
   latestDepthIndex.assign(-1);
   latestRgbIndex.assign(-1);
@@ -36,30 +93,81 @@ RealSenseInterface::RealSenseInterface(int inWidth,int inHeight,int inFps)
     frameBuffers[i] = std::pair<std::pair<uint8_t *,uint8_t *>,int64_t>(std::pair<uint8_t *,uint8_t *>(newDepth,newImage),0);
   }
 
-  setAutoExposure(true);
-  setAutoWhiteBalance(true);
+  auto color_callback = [this](rs2::frame frame) {
+    std::cout << "color" << std::endl;
+    lastRgbTime = std::chrono::duration_cast<std::chrono::milliseconds>(
+        std::chrono::system_clock::now().time_since_epoch()).count();
 
-  rgbCallback = new RGBCallback(lastRgbTime,
-    latestRgbIndex,
-    rgbBuffers);
+    int bufferIndex = (latestRgbIndex.getValue() + 1) % numBuffers;
+    auto image = frame.as<rs2::video_frame>();
+    std::memcpy(rgbBuffers[bufferIndex].first, frame.get_data(),
+		image.get_width() * image.get_height() * 3);
 
-  depthCallback = new DepthCallback(lastDepthTime,
-    latestDepthIndex,
-    latestRgbIndex,
-    rgbBuffers,
-    frameBuffers);
+    rgbBuffers[bufferIndex].second = lastRgbTime;
 
-  dev->set_frame_callback(rs::stream::depth,*depthCallback);
-  dev->set_frame_callback(rs::stream::color,*rgbCallback);
+    latestRgbIndex++;
+  };
 
-  dev->start();
+  auto stereo_callback = [this](rs2::frame frame) {
+    std::cout << "stereo" << std::endl;
+    lastDepthTime = std::chrono::duration_cast<std::chrono::milliseconds>(
+        std::chrono::system_clock::now().time_since_epoch()).count();
+
+    int bufferIndex = (latestDepthIndex.getValue() + 1) % numBuffers;
+
+    auto image = frame.as<rs2::video_frame>();
+    // The multiplication by 2 is here because the depth is actually uint16_t
+    std::memcpy(frameBuffers[bufferIndex].first.first,frame.get_data(),
+        image.get_width() * image.get_height() * 2);
+
+    frameBuffers[bufferIndex].second = lastDepthTime;
+
+    int lastImageVal = latestRgbIndex.getValue();
+
+    if(lastImageVal == -1)
+      {
+        return;
+      }
+
+    lastImageVal %= numBuffers;
+
+    std::memcpy(frameBuffers[bufferIndex].first.second,rgbBuffers[lastImageVal].first,
+		image.get_width() * image.get_height() * 3);
+    
+    latestDepthIndex++;
+  };
+
+  color.start(color_callback);
+  stereo.start(stereo_callback);
+
+  // setAutoExposure(true);
+  // setAutoWhiteBalance(true);
+
+  // rgbCallback = new RGBCallback(lastRgbTime,
+  //   latestRgbIndex,
+  //   rgbBuffers);
+
+  // depthCallback = new DepthCallback(lastDepthTime,
+  //   latestDepthIndex,
+  //   latestRgbIndex,
+  //   rgbBuffers,
+  //   frameBuffers);
+
+  // dev->set_frame_callback(rs::stream::depth,*depthCallback);
+  // dev->set_frame_callback(rs::stream::color,*rgbCallback);
+
+  // dev->start();
 }
 
 RealSenseInterface::~RealSenseInterface()
 {
   if(initSuccessful)
   {
-    dev->stop();
+    // dev->stop();
+    stereo.stop();
+    color.stop();
+    stereo.close();
+    color.stop();
 
     for(int i = 0; i < numBuffers; i++)
     {
@@ -72,29 +180,29 @@ RealSenseInterface::~RealSenseInterface()
       free(frameBuffers[i].first.second);
     }
 
-    delete rgbCallback;
-    delete depthCallback;
   }
 }
 
 void RealSenseInterface::setAutoExposure(bool value)
 {
-  dev->set_option(rs::option::color_enable_auto_exposure,value);
+  // dev->set_option(rs::option::color_enable_auto_exposure,value);
 }
 
 void RealSenseInterface::setAutoWhiteBalance(bool value)
 {
-  dev->set_option(rs::option::color_enable_auto_white_balance,value);
+  // dev->set_option(rs::option::color_enable_auto_white_balance,value);
 }
 
 bool RealSenseInterface::getAutoExposure()
 {
-  return dev->get_option(rs::option::color_enable_auto_exposure);
+  // return dev->get_option(rs::option::color_enable_auto_exposure);
+  return true;
 }
 
 bool RealSenseInterface::getAutoWhiteBalance()
 {
-  return dev->get_option(rs::option::color_enable_auto_white_balance);
+  // return dev->get_option(rs::option::color_enable_auto_white_balance);
+  return true;
 }
 #else
 
--- GUI/src/Tools/RealSenseInterface.h
+++ GUI/src/Tools/RealSenseInterface.h
@@ -6,7 +6,7 @@
 #include <map>
 
 #ifdef WITH_REALSENSE
-#include "librealsense/rs.hpp"
+#include "librealsense2/rs.hpp"
 #endif
 
 #include "ThreadMutexObject.h"
@@ -36,100 +36,102 @@ public:
   }
 
 #ifdef WITH_REALSENSE
-  struct RGBCallback
-  {
-  public:
-    RGBCallback(int64_t & lastRgbTime,
-      ThreadMutexObject<int> & latestRgbIndex,
-      std::pair<uint8_t *,int64_t> * rgbBuffers)
-      : lastRgbTime(lastRgbTime),
-      latestRgbIndex(latestRgbIndex),
-      rgbBuffers(rgbBuffers)
-    {
-    }
+/*   struct RGBCallback */
+/*   { */
+/*   public: */
+/*     RGBCallback(int64_t & lastRgbTime, */
+/*       ThreadMutexObject<int> & latestRgbIndex, */
+/*       std::pair<uint8_t *,int64_t> * rgbBuffers) */
+/*       : lastRgbTime(lastRgbTime), */
+/*       latestRgbIndex(latestRgbIndex), */
+/*       rgbBuffers(rgbBuffers) */
+/*     { */
+/*     } */
 
-    void operator()(rs::frame frame)
-    {
-      lastRgbTime = std::chrono::duration_cast<std::chrono::milliseconds>(
-        std::chrono::system_clock::now().time_since_epoch()).count();
+/*     void operator()(rs2::frame frame) */
+/*     { */
+/*       lastRgbTime = std::chrono::duration_cast<std::chrono::milliseconds>( */
+/*         std::chrono::system_clock::now().time_since_epoch()).count(); */
 
-      int bufferIndex = (latestRgbIndex.getValue() + 1) % numBuffers;
+/*       int bufferIndex = (latestRgbIndex.getValue() + 1) % numBuffers; */
 
-      memcpy(rgbBuffers[bufferIndex].first,frame.get_data(),
-        frame.get_width() * frame.get_height() * 3);
+/*       memcpy(rgbBuffers[bufferIndex].first,frame.get_data(), */
+/*         frame.get_width() * frame.get_height() * 3); */
 
-      rgbBuffers[bufferIndex].second = lastRgbTime;
+/*       rgbBuffers[bufferIndex].second = lastRgbTime; */
 
-      latestRgbIndex++;
-    }
+/*       latestRgbIndex++; */
+/*     } */
 
-  private:
-    int64_t & lastRgbTime;
-    ThreadMutexObject<int> & latestRgbIndex;
-    std::pair<uint8_t *,int64_t> * rgbBuffers;
-  };
+/*   private: */
+/*     int64_t & lastRgbTime; */
+/*     ThreadMutexObject<int> & latestRgbIndex; */
+/*     std::pair<uint8_t *,int64_t> * rgbBuffers; */
+/*   }; */
 
-  struct DepthCallback
-  {
-  public:
-    DepthCallback(int64_t & lastDepthTime,
-      ThreadMutexObject<int> & latestDepthIndex,
-      ThreadMutexObject<int> & latestRgbIndex,
-      std::pair<uint8_t *,int64_t> * rgbBuffers,
-      std::pair<std::pair<uint8_t *,uint8_t *>,int64_t> * frameBuffers)
-      : lastDepthTime(lastDepthTime),
-      latestDepthIndex(latestDepthIndex),
-      latestRgbIndex(latestRgbIndex),
-      rgbBuffers(rgbBuffers),
-      frameBuffers(frameBuffers)
-    {
-    }
+/*   struct DepthCallback */
+/*   { */
+/*   public: */
+/*     DepthCallback(int64_t & lastDepthTime, */
+/*       ThreadMutexObject<int> & latestDepthIndex, */
+/*       ThreadMutexObject<int> & latestRgbIndex, */
+/*       std::pair<uint8_t *,int64_t> * rgbBuffers, */
+/*       std::pair<std::pair<uint8_t *,uint8_t *>,int64_t> * frameBuffers) */
+/*       : lastDepthTime(lastDepthTime), */
+/*       latestDepthIndex(latestDepthIndex), */
+/*       latestRgbIndex(latestRgbIndex), */
+/*       rgbBuffers(rgbBuffers), */
+/*       frameBuffers(frameBuffers) */
+/*     { */
+/*     } */
+    
+/*     void operator()(rs2::frame frame) */
+/*     { */
+/*       lastDepthTime = std::chrono::duration_cast<std::chrono::milliseconds>( */
+/*         std::chrono::system_clock::now().time_since_epoch()).count(); */
 
-    void operator()(rs::frame frame)
-    {
-      lastDepthTime = std::chrono::duration_cast<std::chrono::milliseconds>(
-        std::chrono::system_clock::now().time_since_epoch()).count();
+/*       int bufferIndex = (latestDepthIndex.getValue() + 1) % numBuffers; */
 
-      int bufferIndex = (latestDepthIndex.getValue() + 1) % numBuffers;
+/*       // The multiplication by 2 is here because the depth is actually uint16_t */
+/*       memcpy(frameBuffers[bufferIndex].first.first,frame.get_data(), */
+/*         frame.get_width() * frame.get_height() * 2); */
 
-      // The multiplication by 2 is here because the depth is actually uint16_t
-      memcpy(frameBuffers[bufferIndex].first.first,frame.get_data(),
-        frame.get_width() * frame.get_height() * 2);
+/*       frameBuffers[bufferIndex].second = lastDepthTime; */
 
-      frameBuffers[bufferIndex].second = lastDepthTime;
+/*       int lastImageVal = latestRgbIndex.getValue(); */
 
-      int lastImageVal = latestRgbIndex.getValue();
+/*       if(lastImageVal == -1) */
+/*       { */
+/*         return; */
+/*       } */
 
-      if(lastImageVal == -1)
-      {
-        return;
-      }
+/*       lastImageVal %= numBuffers; */
 
-      lastImageVal %= numBuffers;
+/*       memcpy(frameBuffers[bufferIndex].first.second,rgbBuffers[lastImageVal].first, */
+/*         frame.get_width() * frame.get_height() * 3); */
 
-      memcpy(frameBuffers[bufferIndex].first.second,rgbBuffers[lastImageVal].first,
-        frame.get_width() * frame.get_height() * 3);
+/*       latestDepthIndex++; */
+/*     } */
 
-      latestDepthIndex++;
-    }
+/*   private: */
+/*     int64_t & lastDepthTime; */
+/*     ThreadMutexObject<int> & latestDepthIndex; */
+/*     ThreadMutexObject<int> & latestRgbIndex; */
 
-  private:
-    int64_t & lastDepthTime;
-    ThreadMutexObject<int> & latestDepthIndex;
-    ThreadMutexObject<int> & latestRgbIndex;
-
-    std::pair<uint8_t *,int64_t> * rgbBuffers;
-    std::pair<std::pair<uint8_t *,uint8_t *>,int64_t> * frameBuffers;
-  };
+/*     std::pair<uint8_t *,int64_t> * rgbBuffers; */
+/*     std::pair<std::pair<uint8_t *,uint8_t *>,int64_t> * frameBuffers; */
+/*   }; */
 #endif
 
 private:
 #ifdef WITH_REALSENSE
-  rs::device *dev;
-  rs::context ctx;
+  rs2::device dev;
+  rs2::context ctx;
+  rs2::sensor stereo;
+  rs2::sensor color;
 
-  RGBCallback * rgbCallback;
-  DepthCallback * depthCallback;
+  // RGBCallback * rgbCallback;
+  // DepthCallback * depthCallback;
 #endif
 
   bool initSuccessful;
